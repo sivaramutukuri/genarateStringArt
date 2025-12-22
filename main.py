cf02c8ac0b7223a40c0c53d30ae6d53696c19049
SivaRamUtukuri <sivaramutukuri@gmail.com>
2025-12-22 23:37:03 +0530

    publish issues

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from flask import Flask, jsonify
 from flask_cors import CORS
+from dotenv import load_dotenv
+
 
 from firebase import initialize_firebase
 from v1 import startGenarate
@@ -8,10 +10,12 @@ from v1 import startGenarate
 app = Flask(__name__)
 CORS(app)
 
+load_dotenv()  # loads variables from .env file
 
-initialize_firebase(storage_bucket="your-project-id.appspot.com")
 
+storage_bucket = os.environ.get("FIREBASE_STORAGE_BUCKET")
 
+initialize_firebase(storage_bucket=storage_bucket)
 
 @app.get("/")
 def read_root():
@@ -22,3 +26,8 @@ def read_root():
 @app.post("/generate/<art_id>")
 def generate_string_art(art_id):
     startGenarate(art_id)
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
